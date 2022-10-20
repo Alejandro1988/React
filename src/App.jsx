92336b364749e8dd75df5566334d0ea76c788aeb
Ferretti <alejandro.a.alvarez@despegar.com>
2022-10-19 23:55:10 -0300

    Proyecto inicial con react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <>
+            { console.log("hola") }
+            <div>
+                <h1>hola</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
